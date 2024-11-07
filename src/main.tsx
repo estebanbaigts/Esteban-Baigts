20c70ab157e064fb9c8a60fea9b9091b1da5ca8d
Esteban <91676362+estebanbaigts@users.noreply.github.com>
2024-11-07 10:53:13 +0100

    [ADD] add analytics web for my portfolio with vercel

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from './context/ThemeContext';
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 import App from './App.tsx';
 import './index.css';
 
 inject();
+injectSpeedInsights();
 
 
 createRoot(document.getElementById('root')!).render(
